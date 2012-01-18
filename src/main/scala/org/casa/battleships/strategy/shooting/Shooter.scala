56ed8f8dcd4ffa2d94a32e26db902c7a12ba1370
Joaquin Caraballo <jcaraballo.git@gmail.com>
2012-01-18 23:35:56 +0000

    Convert Shooter from abstract class into trait
    I don't remember why I made this an abstract class in the first place (or if there was a reason)

--- src/main/scala/org/casa/battleships/strategy/shooting/Shooter.scala
+++ src/main/scala/org/casa/battleships/strategy/shooting/Shooter.scala
@@ -2,6 +2,6 @@ package org.casa.battleships.strategy.shooting
 
 import org.casa.battleships.{ShotOutcome, Position}
 
-abstract class Shooter {
+trait Shooter {
   def shoot(shootable: Set[Position], history: List[(Position, ShotOutcome.Value)]): Option[Position]
 }
