bddd9fc6661de3ce74c17a6e3c924d6be7b99c0c
Joaquin Caraballo <jcaraballo.git@gmail.com>
2012-01-20 22:08:36 +0000

    Move actual tests to the beginning of the file to highlight them

--- src/test/scala/org/casa/battleships/strategy/shooting/SequentialShooterTest.scala
+++ src/test/scala/org/casa/battleships/strategy/shooting/SequentialShooterTest.scala
@@ -12,11 +12,36 @@ import org.hamcrest.Matcher
 import org.hamcrest.CoreMatchers.is
 
 class SequentialShooterTest extends FunSuite {
-
   val shootable: Set[Position] = Set[Position](pos(1, 1))
-  val history: List[(Position, ShotOutcome.Value)] = (pos(2, 1), Water) :: (pos(1, 2), Water) :: (pos(2, 2), Water) :: Nil
+  val history: List[(Position, ShotOutcome.Value)] = (pos(2, 1), Water) ::(pos(1, 2), Water) ::(pos(2, 2), Water) :: Nil
+
+  test("fail when all shooters fail") {
+    val multipleShooter: SequentialShooter = new SequentialShooter(failingShooter, failingShooter, failingShooter)
+    assertThat(multipleShooter.shoot(shootable, history), isNone)
+  }
+
+  test("call all shooters until one suceeds") {
+    val shooter1 = failingShooter
+    val shooter2 = failingShooter
+    val shooter3 = shooterThatShootsAt(somePosition)
+    val shooter4 = mock(classOf[Shooter])
+
+    new SequentialShooter(shooter1, shooter2, shooter3, shooter4).shoot(shootable, history)
+
+    val sequentially: InOrder = inOrder(shooter1, shooter2, shooter3)
+    sequentially.verify(shooter1).shoot(anyShootable, anyHistory)
+    sequentially.verify(shooter2).shoot(anyShootable, anyHistory)
+    sequentially.verify(shooter3).shoot(anyShootable, anyHistory)
+    verify(shooter4, never()).shoot(anyShootable, anyHistory)
+  }
+
+  test("returns what the first succeeding shooter returns") {
+    val multipleShooter = new SequentialShooter(failingShooter, failingShooter, shooterThatShootsAt(somePosition), shooterThatShootsAt(anotherPosition))
+    assertThat(multipleShooter.shoot(shootable, history), is[Option[Position]](somePosition))
+  }
 
   def anyShootable: Set[Position] = anyObject()
+
   def anyHistory: List[(Position, ShotOutcome.Value)] = anyObject()
 
   def failingShooter: Shooter = {
@@ -41,30 +66,5 @@ class SequentialShooterTest extends FunSuite {
 
   def isNone: Matcher[Option[Position]] = {
     is[Option[Position]](None)
-  }
-  
-  test("fail when all shooters fail"){
-    val multipleShooter: SequentialShooter = new SequentialShooter(failingShooter, failingShooter, failingShooter)
-    assertThat(multipleShooter.shoot(shootable, history), isNone)
-  }
-  
-  test("call all shooters until one suceeds"){
-    val shooter1 = failingShooter
-    val shooter2 = failingShooter
-    val shooter3 = shooterThatShootsAt(somePosition)
-    val shooter4 = mock(classOf[Shooter])
-
-    new SequentialShooter(shooter1, shooter2, shooter3, shooter4).shoot(shootable, history)
-
-    val sequentially: InOrder = inOrder(shooter1, shooter2, shooter3)
-    sequentially.verify(shooter1).shoot(anyShootable, anyHistory)
-    sequentially.verify(shooter2).shoot(anyShootable, anyHistory)
-    sequentially.verify(shooter3).shoot(anyShootable, anyHistory)
-    verify(shooter4, never()).shoot(anyShootable, anyHistory)
-  }
-  
-  test("returns what the first succeeding shooter returns"){
-    val multipleShooter = new SequentialShooter(failingShooter, failingShooter, shooterThatShootsAt(somePosition), shooterThatShootsAt(anotherPosition))
-    assertThat(multipleShooter.shoot(shootable, history), is[Option[Position]](somePosition))
   }
 }
