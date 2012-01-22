489ccc3ce5b9b28ba57bb8d634b31202b17acf26
Joaquin Caraballo <jcaraballo.git@gmail.com>
2012-01-22 23:53:42 +0000

    createHistoryOfWater: Generates history from ascii representation

--- src/test/scala/functional/GameFunctionalTest.scala
+++ src/test/scala/functional/GameFunctionalTest.scala
@@ -9,8 +9,10 @@ import org.junit.Assert.assertThat
 import org.hamcrest.CoreMatchers.is
 import org.hamcrest.CoreMatchers.anyOf
 import org.casa.battleships.strategy.shooting.Shooters.bestShooter
-import collection.immutable.Set
-import org.casa.battleships.{Positions, Turn, ComputerPlayer}
+import collection.Iterator
+import collection.immutable.IndexedSeq
+import util.matching.Regex.MatchIterator
+import org.casa.battleships._
 
 class GameFunctionalTest extends FunSuite {
   test("Plays game where user starts") {
@@ -21,11 +23,53 @@ class GameFunctionalTest extends FunSuite {
     assertThat(computerPlayer.play(Turn(Hit, pos(1, 1))), is(Turn(Hit, pos(1, 1))))
   }
 
+  def createHistoryOfWater(history: String): List[(Position, ShotOutcome.Value)] = {
+    val lines: MatchIterator = """\{[^}]*\}""".r.findAllIn(history)
+    val stringRepresentationOfPositionsWithZeroBasedColumn: Iterator[IndexedSeq[(Char, Int)]] =
+      lines.map(_.zipWithIndex.filter(((c: Char, i: Int) => i % 2 != 0).tupled).map(((c: Char, i: Int) => c).tupled).zipWithIndex)
+    val charactersWithPositions: Iterator[(Char, Position)] = stringRepresentationOfPositionsWithZeroBasedColumn.zipWithIndex.flatMap(
+      ((v: IndexedSeq[(Char, Int)], i: Int) => v.map((t: (Char, Int)) => (t._1, pos(t._2 + 1, i + 1)))).tupled
+    )
+    val waterPositions: Iterator[Position] = charactersWithPositions.filter((t: (Char, Position)) => t._1 == '·').map((t: (Char, Position)) => t._2)
+    waterPositions.map((_, Water)).toList
+  }
+
   test("Finds immaculate ship when there is only one possible place for it") {
     val computerPlayer = new ComputerPlayer(new UpmostAndThenLeftmostPositionChooser, bestShooter, 10, 5 :: Nil)
 
-    val spaceForShip = Set(pos(1, 1), pos(2, 1), pos(3, 1), pos(4, 1), pos(5, 1))
-    computerPlayer.historyOfMyShotsAtTheEnemy = (Positions.createGrid(10) -- spaceForShip).map((_, Water)).toList
+//    val history = """
+//  1 2 3 4 5 6 7 8 9 0
+//  ~~~~~~~~~~~~~~~~~~~
+//1{          · · · · ·}1
+//2{· · · · · · · · · ·}2
+//3{  ·   ·   ·   ·   ·}3
+//4{·   ·   ·   ·   ·  }4
+//5{  ·   ·   ·   ·   ·}5
+//6{·   ·   ·   ·   ·  }6
+//7{  ·   ·   ·   ·   ·}7
+//8{·   ·   ·   ·   ·  }8
+//9{  ·   ·   ·   ·   ·}9
+//0{·   ·   ·   ·   ·  }0
+//  ~~~~~~~~~~~~~~~~~~~
+//  1 2 3 4 5 6 7 8 9 0
+//"""
+    val history = """
+  1 2 3 4 5 6 7 8 9 0
+  ~~~~~~~~~~~~~~~~~~~
+1{          · · · · ·}1
+2{· · · · · · · · · ·}2
+3{· · · · · · · · · ·}3
+4{· · · · · · · · · ·}4
+5{· · · · · · · · · ·}5
+6{· · · · · · · · · ·}6
+7{· · · · · · · · · ·}7
+8{· · · · · · · · · ·}8
+9{· · · · · · · · · ·}9
+0{· · · · · · · · · ·}0
+  ~~~~~~~~~~~~~~~~~~~
+  1 2 3 4 5 6 7 8 9 0
+"""
+    computerPlayer.historyOfMyShotsAtTheEnemy = createHistoryOfWater(history)
 
     assertThat(computerPlayer.playFirstTurn(pos(1, 1)).shotBack, anyOf(is(pos(1, 1)), is(pos(2, 1)), is(pos(3, 1)), is(pos(4, 1)), is(pos(5, 1))))
   }
