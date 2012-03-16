b0528949533fad4c8e8ed6efeb6d2c5d6057476c
Joaquin Caraballo <jcaraballo.git@gmail.com>
2012-03-16 11:12:22 +0000

    Rewrite Picky using PartialOrdering
    thanks to Travis Brown (see http://stackoverflow.com/a/9657791/1260416 )

--- src/main/scala/org/casa/battleships/fleet/Picky.scala
+++ src/main/scala/org/casa/battleships/fleet/Picky.scala
@@ -1,15 +1,15 @@
 package org.casa.battleships.fleet
 
 
-import collection.immutable.Set
-
-case class Picky[T] private(sets: Set[Set[T]]) {
-  def +(s: Set[T]): Picky[T] = Picky(Picky.internalAddition(this.sets, s))
+case class Picky[T] private(xs: Seq[T])(partialOrdering: PartialOrdering[T]) {
+  def +(s: T): Picky[T] = new Picky(Picky.internalAddition(xs, s)(partialOrdering))(partialOrdering)
 }
 
 object Picky {
-  def apply[T](sets: Set[T]*): Picky[T] = Picky((Set[Set[T]]() /: sets)(internalAddition(_, _)))
+  def picky[T](xs: T*)(implicit partialOrdering: PartialOrdering[T]): Picky[T] = {
+    new Picky((Seq[T]() /: xs)((x: Seq[T], y: T) => internalAddition(x, y)(partialOrdering)))(partialOrdering)
+  }
 
-  private def internalAddition[T](c: Set[Set[T]], s: Set[T]): Set[Set[T]] =
-    if (c.exists(s subsetOf _)) c else c.filterNot(_ subsetOf s) + s
+  private def internalAddition[T](xs: Seq[T], x: T)(partialOrdering: PartialOrdering[T]): Seq[T] =
+    if (xs.exists(partialOrdering.lteq (x, _))) xs else xs.filterNot(partialOrdering.lteq(_, x)) :+ x
 }
--- src/test/scala/org/casa/battleships/fleet/PickyTest.scala
+++ src/test/scala/org/casa/battleships/fleet/PickyTest.scala
@@ -2,21 +2,34 @@ package org.casa.battleships.fleet
 
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
+import Picky.picky
 
 class PickyTest extends FunSuite with ShouldMatchers {
+  implicit def subsetOrdering[A] = new PartialOrdering[Set[A]] {
+    def tryCompare(x: Set[A], y: Set[A]) =
+      if (x == y) Some(0)
+      else if (x subsetOf y) Some(-1)
+      else if (y subsetOf x) Some(1)
+      else None
+
+    def lteq(x: Set[A], y: Set[A]) =
+      this.tryCompare(x, y).map(_ <= 0).getOrElse(false)
+  }
+
   test("Ignores additions of elements that are subsets of present elements") {
-    Picky(Set(1, 2)) + Set(1) should equal(Picky(Set(1, 2)))
+    val actual = picky(Set(1, 2)) + Set(1)
+    actual should equal(picky(Set(1, 2)))
   }
 
   test("Adding an element replaces its subsets") {
-    Picky(Set(1)) + Set(1, 2) should equal(Picky(Set(1, 2)))
+    picky(Set(1)) + Set(1, 2) should equal(picky(Set(1, 2)))
   }
 
   test("Adding an element that doesn't subset and isn't subset of any of the elements adds it") {
-    Picky(Set(1, 3)) + Set(1, 2) should equal(Picky(Set(1, 3), Set(1, 2)))
+    picky(Set(1, 3)) + Set(1, 2) should equal(picky(Set(1, 3), Set(1, 2)))
   }
 
   test("Elements that are subsets of other elements are disregarded") {
-    Picky(Set(1, 2), (Set(1))) should equal(Picky(Set(1, 2)))
+    picky(Set(1, 2), (Set(1))) should equal(picky(Set(1, 2)))
   }
 }
