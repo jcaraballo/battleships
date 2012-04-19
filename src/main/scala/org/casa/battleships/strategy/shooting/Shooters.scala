5d405df2a6362e0c47a511936431af35728e9453
Joaquin Caraballo <jcaraballo.git@gmail.com>
2012-04-19 20:03:58 +0100

    Return trait Shooter
    instead of implementation class in Shooters.bestShooter

--- src/main/scala/org/casa/battleships/strategy/shooting/Shooters.scala
+++ src/main/scala/org/casa/battleships/strategy/shooting/Shooters.scala
@@ -3,7 +3,7 @@ package org.casa.battleships.strategy.shooting
 import org.casa.battleships.strategy.positionchoice.PositionChooser
 
 object Shooters {
-  def bestShooter(positionChooser: PositionChooser): SequentialShooter = {
+  def bestShooter(positionChooser: PositionChooser): Shooter = {
     new SequentialShooter(
       new LinesShooter(positionChooser),
       new AimAtNextToHitShooter(positionChooser),
